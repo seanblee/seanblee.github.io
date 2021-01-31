cd1cf881d7f1083b0045abd918904e876763536c
seanblee <seanlee1997@live.com>
2021-01-30 22:57:21 -1000

    mobile fixes

--- src/sections/Work.js
+++ src/sections/Work.js
@@ -20,6 +20,7 @@ class Work extends Component {
     render() {
         return (
                 <Section id="work">
+                <div className="desktop-work">
                 <Flex
                 width="80%"
                 alignSelf="center"
@@ -85,9 +86,9 @@ class Work extends Component {
                     flexDirection="column"
                     justifyContent="center"
                     alignItems="center">
-                        <span className="TimelineSeparator"/>
+                        <span className="timeline-separator"/>
                         <Image src={micro} className={inView? "WorkIcon" : "WorkContentHidden"} height='100px' width="100px" style={{border: '5px solid #153b50', borderRadius: '50%', objectFit: 'contain'}}/>
-                        <span className="TimelineSeparator"/>
+                        <span className="timeline-separator"/>
                     </Flex>
                     <Flex
                     width={9/21}
@@ -111,9 +112,9 @@ class Work extends Component {
                     flexDirection="column"
                     justifyContent="center"
                     alignItems="center">
-                        <span className="TimelineSeparator"/>
+                        <span className="timeline-separator"/>
                         <Image src={wyndo} className={inView? "WorkIcon" : "WorkContentHidden"} height='100px' width="100px" style={{border: '5px solid #153b50', borderRadius: '50%', objectFit: 'contain'}}/>
-                        <span className="TimelineSeparator"/>
+                        <span className="timeline-separator"/>
                     </Flex>
                     <Flex
                     width={9/21}
@@ -197,9 +198,9 @@ class Work extends Component {
                     flexDirection="column"
                     justifyContent="center"
                     alignItems="center">
-                        <span className="TimelineSeparator"/>
+                        <span className="timeline-separator"/>
                         <Image src={micro} className={inView? "WorkIcon" : "WorkContentHidden"} height='100px' width="100px" style={{border: '5px solid #153b50', borderRadius: '50%', objectFit: 'contain'}}/>
-                        <span className="TimelineSeparator"/>
+                        <span className="timeline-separator"/>
                     </Flex>
                     <Flex
                     width={9/21}
@@ -223,9 +224,9 @@ class Work extends Component {
                     flexDirection="column"
                     justifyContent="center"
                     alignItems="center">
-                        <span className="TimelineSeparator"/>
+                        <span className="timeline-separator"/>
                         <Image src={uoft} className={inView? "WorkIcon" : "WorkContentHidden"} height='100px' width="100px" style={{border: '5px solid #153b50', borderRadius: '50%', objectFit: 'contain'}}/>
-                        <span className="TimelineSeparator"/>
+                        <span className="timeline-separator"/>
                     </Flex>
                     <Flex
                     width={9/21}
@@ -266,6 +267,182 @@ class Work extends Component {
                 </Flex>
                 )}</InView>
                 </Flex>
+                </div>
+                <div className="mobile-work">
+                <Flex
+                width="80%"
+                alignSelf="center"
+                alignItems="center"
+                flexDirection="column"
+                justifyContent="center"
+                my = "8vw">
+                <Flex
+                flexDirection="column"
+                width="fit-content"
+                mb="5vw">
+                    <Text
+                    color="#042a2b"
+                    fontFamily="Segoe"
+                    fontSize="55px"
+                    fontWeight="bolder">
+                        Places I've Worked
+                    </Text>
+                    <span className="orange-underline"/>
+                </Flex>
+                <InView>{({inView, ref}) => (
+                <Flex
+                width="100%"
+                ref={ref}
+                justifyContent="center">
+                        <Flex
+                        className={inView ? "WorkContent" : "WorkContentHidden"}
+                        flexDirection="column"
+                        backgroundColor="#EF7B45"
+                        style={{boxShadow: '1px 2px 4px rgba(0,0,0,0.2)',  borderRadius: '5px' }}
+                        p="2vw">
+                            <Text
+                            fontFamily="consolas"
+                            color="white">
+                                Software Engineer
+                            </Text>
+                            <Text
+                            fontFamily="Segoe"
+                            fontWeight="bolder"
+                            fontSize="23px"
+                            color="white"
+                            my="1px">
+                                Microsoft, 1ES CloudTest
+                            </Text>
+                            <Text
+                            my="6px"
+                            color="white">
+                                CloudTest part of an internal engineering systems team which delivers a distributable testing platform for users on Microsoft's internal DevOps tool.
+                                My latest project involved migrating test systems for large Microsoft teams to Azure.
+                            </Text>
+                            <Flex
+                            flexDirection="row"
+                            flexWrap="wrap">
+                            {this.renderTags(["Azure", "Kubernetes", "MySQL", "CosmosDB", "C#"])}
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                )}</InView>
+                <span className="timeline-separator-mobile"/>
+                <InView>{({inView, ref}) => (
+                <Flex
+                width="100%"
+                ref={ref}
+                justifyContent="center">
+                        <Flex
+                        flexDirection="column"
+                        backgroundColor="#cdedf6"
+                        className={inView ? "WorkContent" : "WorkContentHidden"}
+                        style={{boxShadow: '1px 2px 4px rgba(0,0,0,0.2)',  borderRadius: '5px' }}
+                        p="2vw">
+                            <Text
+                            fontFamily="consolas"
+                            color="#042a2b">
+                                Chief Technical Officer
+                            </Text>
+                            <Text
+                            fontFamily="Segoe"
+                            fontWeight="bolder"
+                            fontSize="23px"
+                            color="#042a2b"
+                            my="1px">
+                                Wyndo
+                            </Text>
+                            <Text
+                            my="6px"
+                            color="#042a2b">
+                                Wyndo is an insurtech platform for automatically processing insurance documents and providing smart suggestions and requotes. I managed the software team and was in charge of the development and design of the platform. I left the team after we got into an accelerator in the UK with a grant of $30,000.
+                            </Text>
+                            <Flex
+                            flexDirection="row"
+                            flexWrap="wrap">
+                            {this.renderTags(["React", "Node", "Express", "Python", "OpenCV", "GCP"])}
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                )}</InView>
+                <span className="timeline-separator-mobile"/>
+                <InView>{({inView, ref}) => (
+                <Flex
+                width="100%"
+                ref={ref}
+                justifyContent="center">
+                        <Flex
+                        className={inView ? "WorkContent" : "WorkContentHidden"}
+                        flexDirection="column"
+                        backgroundColor="#cdedf6"
+                        style={{boxShadow: '1px 2px 4px rgba(0,0,0,0.2)',  borderRadius: '5px' }}
+                        p="2vw">
+                            <Text
+                            fontFamily="consolas"
+                            color="#153b50">
+                                Software Engineering Intern
+                            </Text>
+                            <Text
+                            fontFamily="Segoe"
+                            fontWeight="bolder"
+                            fontSize="23px"
+                            color="#153b50"
+                            my="1px">
+                                Microsoft, MCIO
+                            </Text>
+                            <Text
+                            my="6px"
+                            color="#153b50">
+                                I did my junior internship at Microsoft where I worked on a service that allows data warehouse operators to manage hardware metadata. 
+                            </Text>
+                            <Flex
+                            flexDirection="row"
+                            flexWrap="wrap">
+                            {this.renderTags(["Azure", "MySQL", "C#", "ASP.NET"])}
+                            </Flex>
+                        </Flex>
+                </Flex>
+                )}</InView>
+                <span className="timeline-separator-mobile"/>
+                <InView>{({inView, ref}) => (
+                <Flex
+                width="100%"
+                ref={ref}
+                justifyContent="center">
+                        <Flex
+                        flexDirection="column"
+                        backgroundColor="#153b50"
+                        className={inView ? "WorkContent" : "WorkContentHidden"}
+                        style={{boxShadow: '1px 2px 4px rgba(0,0,0,0.2)',  borderRadius: '5px' }}
+                        p="2vw">
+                            <Text
+                            fontFamily="consolas"
+                            color="#cdedf6">
+                                Engineering Outreach Instructor
+                            </Text>
+                            <Text
+                            fontFamily="Segoe"
+                            fontWeight="bolder"
+                            fontSize="23px"
+                            color="#cdedf6"
+                            my="1px">
+                                University of Toronto                                                              
+                            </Text>
+                            <Text
+                            my="6px"
+                            color="#cdedf6">
+                                Wyndo is an insurtech platform for automatically processing insurance documents and providing smart suggestions and requotes. I managed the software team and was in charge of the development and design of the platform. I left the team after we got into an accelerator in the UK with a grant of $30,000.
+                            </Text>
+                            <Flex
+                            flexDirection="row"
+                            flexWrap="wrap">
+                            {this.renderTags(["React", "Node", "Express", "Python", "OpenCV", "GCP"])}
+                            </Flex>
+                        </Flex>
+                </Flex>
+                )}</InView>
+                </Flex>
+                </div>
             </Section>
         );
     }
